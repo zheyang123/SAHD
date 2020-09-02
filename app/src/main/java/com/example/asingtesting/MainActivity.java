73a5d8fd628a83aca64e808b2c8364e8d710d114
idontknow-howtocode <addyyyw-wm19@student.tarc.edu.my>
2020-09-02 21:22:26 +0800

    Commit by addy

--- app/src/main/java/com/example/asingtesting/MainActivity.java
+++ app/src/main/java/com/example/asingtesting/MainActivity.java
@@ -1,22 +1,47 @@
 package com.example.asingtesting;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private static final int Result_Image = 1;
+    ImageView imageView;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         int abd;
-         abd = 12;
-String qsw = "123qdas";
-     //hello cannot
-        //haha
+        imageView = (ImageView) findViewById(R.id.image1);
+        button = (Button) findViewById(R.id.uploadimage);
+        imageView.setOnClickListener(this);
+        button.setOnClickListener(this);
+    }
 
-        String w = "123qdas";
-        //test2020
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.image1:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, Result_Image);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==Result_Image && resultCode==RESULT_OK && data!=null) {
+            Uri selected_image = data.getData();
+            imageView.setImageURI(selected_image);
+        }
+
     }
 }
