39e0314a41d70a0fc08489d4105f4306a4243541
artoria0210 <66988774+artoria0210@users.noreply.github.com>
2020-09-15 05:40:29 +0800

    lololol

--- app/src/main/java/com/example/asingtesting/MainActivity.java
+++ app/src/main/java/com/example/asingtesting/MainActivity.java
@@ -1,121 +1,167 @@
 package com.example.asingtesting;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Patterns;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.basgeekball.awesomevalidation.AwesomeValidation;
-import com.basgeekball.awesomevalidation.ValidationStyle;
-import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.regex.Pattern;
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "TAG";
+    private EditText Password1,Password2,Password3;
+    private Button login,logout;
+    private Button register;
+    private EditText Email1,Email2;
+    String userID,custemail;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    FirebaseAuth mFirebaseAuth;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    //Initialize Variable
-    EditText companyNameTxt,companyAddressTxt,poscodeTxt,companyTypeTxt,companyEmailTxt,
-            CompanyOperatingHourTxt,companyWorkingDateTxt;
-    Button register;
-    AwesomeValidation awesomeValidation;
-    ImageView companyLogo ;
-    private static final int image = 1;
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId())
-        { case R.id.Company_Logo:
-            Intent intent = new Intent(
-              Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-            );
-            startActivityForResult(intent,image);
-            break;}
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == image && resultCode == RESULT_OK && data != null)
-        {
-            Uri uri = data.getData();
-            companyLogo.setImageURI(uri);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int img = R.drawable.camara;
-        Intent intent = new Intent(this,login.class);
-        startActivity(intent);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        Password1 = (EditText) findViewById(R.id.editTextTextPassword);
+        Password2 = (EditText) findViewById(R.id.editTextTextPassword2);
+        Password3 = (EditText) findViewById(R.id.editTextTextPassword3);
+        Email1 = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        Email2 = (EditText) findViewById(R.id.editTextTextEmailAddress2);
+        login = (Button) findViewById(R.id.loginbutton);
+        register = (Button) findViewById(R.id.registerbutton);
+        //SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+        //String checkbox = preferences.getString("stayLogged", "");
+        //if (checkbox.equals("false")){
+        // Logout
+        //FirebaseAuth.getInstance().signOut();
+        // Set stay logged checkbox
+        //SharedPreferences.Editor editor = preferences.edit();
+        //editor.putString("stayLogged","true");
+        // editor.apply();
+        //}
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = Email1.getText().toString();
+                String pass = Password1.getText().toString();
+                if (email.isEmpty()) {
+                    Email1.setError("Please enter your email!!!!");
+                    Email1.requestFocus();
+                } else if (pass.isEmpty()) {
+                    Password1.setError("Please enter your password!!!!");
+                    Password1.requestFocus();
+                } else {
 
-
-        //Assign Variable
-        companyNameTxt = findViewById(R.id.company_name_txt);
-        companyAddressTxt = findViewById(R.id.company_address_txt);
-        poscodeTxt = findViewById(R.id.poscode_txt);
-        companyTypeTxt = findViewById(R.id.company_type_txt);
-        companyEmailTxt = findViewById(R.id.company_Email_txt);
-        CompanyOperatingHourTxt = findViewById(R.id.Company_Operating_Hour_txt);
-        companyWorkingDateTxt = findViewById(R.id.company_working_date_txt);
-        register = findViewById(R.id.Register);
-        companyLogo = findViewById(R.id.Company_Logo);
-        companyLogo.setImageResource(img);
-
-        //Initialize Validation
-        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
-
-        //Add Validation
-        //Name
-        awesomeValidation.addValidation(this,R.id.company_name_txt, RegexTemplate.NOT_EMPTY,R.string.invalid_name);
-        //Phone Number
-        awesomeValidation.addValidation(this,R.id.company_Email_txt, Patterns.EMAIL_ADDRESS,R.string.invalid_email);
-        //Email Address
-        awesomeValidation.addValidation(this,R.id.company_address_txt,RegexTemplate.NOT_EMPTY,R.string.invalid_Email_Address);
-        //Poscode
-        awesomeValidation.addValidation(this,R.id.poscode_txt,".{5,}",R.string.invalid_poscode);
-        //Company Type
-        awesomeValidation.addValidation(this,R.id.company_type_txt,RegexTemplate.NOT_EMPTY,R.string.invalid_company_type);
+                    mFirebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Intent loginSuccessful = new Intent(MainActivity.this, MainStore.class);//go item page after login
+                                //startActivity(loginSuccessful);
+                                Toast.makeText(MainActivity.this, "Login Successfull!!!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Login Failed!!! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                //progressBar.setVisibility(View.GONE);
+                                //bL.setVisibility(View.VISIBLE);
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String companyname,companyaddress,companytype,companyemail,companyoperatinghour,companyworkingdate;
-                int companyposcode;
-                companyname = companyNameTxt.getText().toString();
-                companyaddress = companyAddressTxt.getText().toString();
-                companytype = companyTypeTxt.getText().toString();
-                companyemail = companyEmailTxt.getText().toString();
-                companyoperatinghour = CompanyOperatingHourTxt.getText().toString();
-                companyworkingdate = companyWorkingDateTxt.getText().toString();
-                companyposcode = Integer.parseInt(poscodeTxt.getText().toString());
-                //check Validation
-                if (awesomeValidation.validate()){
-                    Toast.makeText(getApplicationContext(),"Register Successfull",Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(getApplicationContext(),"Register Failed",Toast.LENGTH_SHORT).show();
-                }
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("company Details");
-                companyRegisterClass companyRegister = new companyRegisterClass();
-                companyRegister.companyRegister(companyname,companyaddress,companytype,companyemail,companyoperatinghour,companyworkingdate,companyposcode);
-                DatabaseReference newRef = myRef.push();
-                newRef.setValue(companyRegister);
+
+                custemail=Email2.getText().toString();
+                String pass1=Password2.getText().toString();
+                String pass2=Password3.getText().toString();
+                if(custemail.isEmpty())
+                {Email2.setError("Please enter your phone number!!!!");
+                    Email2.requestFocus();}
+                else if(pass1.isEmpty())
+                {Password2.setError("Please enter your password!!!!");
+                    Password2.requestFocus();}
+                else if(pass2.isEmpty())
+                {Password3.setError("Please enter your password!!!!");
+                    Password3.requestFocus();}
+                else if(!pass1.matches("^[a-zA-Z0-9]*$"))
+                {Password2.setError("Please try another password!!!!");
+                    Password2.requestFocus();}
+                else if(pass1.length() < 6)
+                {Password2.setError("Your password is too short.Please try again!!!!");
+                    Password2.requestFocus();}
+                else if(pass1.length() > 14)
+                {Password2.setError("Your password is too long.Please try again!!!!");
+                    Password2.requestFocus();}
+                else if(!pass1.equals(pass2))
+                {Password3.setError("Your password is not match.Please try again!!!!");
+                    Password3.requestFocus();}
+                else
+                {
+                    mFirebaseAuth.createUserWithEmailAndPassword(custemail,pass1).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()){
+                                Toast.makeText(MainActivity.this,"SignUp Unsuccessful!!! " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                                //progressBar.setVisibility(View.GONE);
+                                //bCA.setVisibility(View.VISIBLE);
+                                //t.setVisibility(View.VISIBLE);
+                                //bSI.setVisibility(View.VISIBLE);
+                            }else{
+                                FirebaseUser fUser = mFirebaseAuth.getCurrentUser();
+                                fUser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(MainActivity.this, "Verification Email Has been Sent.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d(TAG, "onFailure: Email not sent " + e.getMessage());
+                                    }
+                                });
+                                Toast.makeText(MainActivity.this, "User Created.", Toast.LENGTH_SHORT).show();
+                                userID = mFirebaseAuth.getCurrentUser().getUid();
+                                database = FirebaseDatabase.getInstance();
+                                //Write into realtime database
+                                myRef = database.getReference("customer").child(userID);
+
+                                myRef.child("userID").setValue(userID);
+                                myRef.child("email").setValue(custemail);
+
+                            }
+                        }
+                    });
+                    Toast.makeText(MainActivity.this,"Done",Toast.LENGTH_SHORT).show();}
             }
         });
-        companyLogo.setOnClickListener(this);
+
 
     }
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        finish();
+    }
+
 }
